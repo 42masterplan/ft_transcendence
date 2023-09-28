f6b212a8c1df18750e4fd13102bf1e9759ef6d63
daejlee <savif4991@gmail.com>
2023-09-28 16:07:57 +0900

    feature: shadcn 컴포넌트들 추가 설치 및 button.tsx css 변수 추가

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -25,7 +25,9 @@ const buttonVariants = cva(
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
-        navBtn: 'h-1/7 w-1/6 '
+        navBtn: 'h-1/7 w-1/6',
+        check: 'h-10 w-28',
+        long: 'text-7xl'
       }
     },
     defaultVariants: {
