008b64925a4267be2235ac11983a43c38ca2dc77
wnddms12345 <wnddms12345@naver.com>
2023-10-02 21:46:46 +0900

    Feat: Last Avator Click to Upload - 마지막 아바타를 클릭시 업로드 가능하게 변경했습니다. - 아바타 미리보기를 구현하려고 했는데 구현하다가 너무 코드가 길어져서   나중에 state 넣을 때 넣는 방향으로 바꾸는 게 나을 것 같아 일단   이정도로 하겠습니다.

--- frontend/src/pages/welcome/register/AvatarContainer.tsx
+++ frontend/src/pages/welcome/register/AvatarContainer.tsx
@@ -23,24 +23,36 @@ export default function AvatarContainer() {
     'shark_health',
     'sloth_health'
   ];
+
+  function uploadHandler(id: string) {
+    if (id === '15') {
+      const realUpload = document.getElementById(`fileUpload-${id}`);
+      if (realUpload) {
+        realUpload.click();
+      }
+    }
+  }
+
   const renderAvatarContainer = () => {
     const rows = [];
     for (let row = 0; row < 4; row++) {
       const rowSquares = [];
       for (let col = 0; col < 4; col++) {
         const idx = row * 4 + col;
+        const id = idx.toString();
         rowSquares.push(
           <div className='flex items-center space-x-2'>
             <RadioGroupItem
-              value={idx.toString()}
-              id={idx.toString()}
+              value={id}
+              id={id}
               className='peer sr-only'
+              onClick={() => uploadHandler(id)}
             />
             <Label
               htmlFor={idx.toString()}
               className='flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary'
             >
-              <AvatarIcon key={idx} avatarName={AvatarList[idx]} />
+              <AvatarIcon key={id} avatarName={AvatarList[idx]} />
             </Label>
           </div>
         );
@@ -55,6 +67,13 @@ export default function AvatarContainer() {
   };
   return (
     <>
+      <input
+        type='file'
+        id={'fileUpload-15'}
+        className='forUpload hidden'
+        accept='image/*'
+        required
+      ></input>
       <RadioGroup defaultValue='1' className='grid gap-4'>
         {renderAvatarContainer()}
       </RadioGroup>
--- frontend/src/pages/welcome/register/InputFile.tsx
+++ frontend/src/pages/welcome/register/InputFile.tsx
@@ -1,11 +1,13 @@
-import { Input } from "@/components/components/shadcn/input"
-import { Label } from "@/components/components/shadcn/label"
- 
+import {Input} from '@/components/components/shadcn/input';
+import {Label} from '@/components/components/shadcn/label';
+
 export function InputFile() {
   return (
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="커스텀 아바타 직접 올리기">커스텀 아바타 직접 올리기</Label>
-      <Input id="picture" type="file" />
+    <div className='grid w-full max-w-sm items-center gap-1.5'>
+      <Label htmlFor='커스텀 아바타 직접 올리기'>
+        커스텀 아바타 직접 올리기
+      </Label>
+      <Input id='picture' type='file' />
     </div>
-  )
+  );
 }
