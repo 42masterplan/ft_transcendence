16456f5675bc38056ea2f43361b3838661eace7e
Hawardshin <wnddms12345@naver.com>
2023-10-05 22:21:29 +0900

    Feat: Header In all Page - 헤더를 모든 페이지에 추가했습니다. - 이건 추후에 필요없는 페이지는 Layout사용해서 변경하는 식으로 가면 될듯..!

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,9 @@
 import {Button} from '@/components/shadcn/button';
 import Link from 'next/link';
-import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <h1 className='text-center'>Main Page</h1>
       <div className='bg-red-500 h-2'></div>
       <Button variant='ghost' className='bg-red-400'>
