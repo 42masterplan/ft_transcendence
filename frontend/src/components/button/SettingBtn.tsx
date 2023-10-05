131acd9ec9c6ed9c98e6f5dff6b024a0d6a47834
Hawardshin <wnddms12345@naver.com>
2023-10-05 13:21:06 +0900

    Feat: Setting Btn pos and scroll - 세팅 버튼 위치 수정하고 스크롤 추가했습니다.

--- frontend/src/components/button/SettingBtn.tsx
+++ frontend/src/components/button/SettingBtn.tsx
@@ -7,26 +7,30 @@ import {
   DialogTrigger
 } from '@/components/shadcn/dialog';
 import ImageBtn from './ImageBtn';
-import {Button} from '../shadcn/button';
-import {InputForm} from '../input/InputForm';
 import UserInfo from '@/components/userInfo/UserInfo';
-import AvatarContainer from '@/components/avatar/AvatarContainer';
 export default function SettingBtn() {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <ImageBtn btn_type='HeaderBtn' file='setting' width={50} height={50} />
-      </DialogTrigger>
-      <DialogContent className='sm:max-w-[425px] bg-color_3'>
-        <DialogHeader>
-          <DialogTitle className='text-center text-4xl'>
-            회원 정보 설정
-          </DialogTitle>
-          <DialogDescription>
-            <UserInfo />
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+    <span className='fixed top-2.5 right-2.5'>
+      <Dialog>
+        <DialogTrigger asChild>
+          <ImageBtn
+            btn_type='HeaderBtn'
+            file='setting'
+            width={50}
+            height={50}
+          />
+        </DialogTrigger>
+        <DialogContent className='sm:max-w-[425px] max-h-screen bg-color_3 overflow-y-auto'>
+          <DialogHeader>
+            <DialogTitle className='text-center text-4xl '>
+              회원 정보 설정
+            </DialogTitle>
+            <DialogDescription>
+              <UserInfo />
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </span>
   );
 }
