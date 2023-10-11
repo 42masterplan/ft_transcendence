d7e2a13f0c4e30456143633f2290849681bb0d6e
Hawardshin <wnddms12345@naver.com>
2023-10-11 18:21:21 +0900

    Feat: 사용하는 것만 냅두고 나머진 지움 - 코드를 조금 더 이쁘게 만들었습니다. - 함수로 쪼갬~

--- frontend/src/components/channel/DropDownAvatarBtn.tsx
+++ frontend/src/components/channel/DropDownAvatarBtn.tsx
@@ -1,24 +1,11 @@
-import {
-  Cloud,
-  Github,
-  Keyboard,
-  LifeBuoy,
-  LogOut,
-  Mail,
-  MessageSquare,
-  Plus,
-  PlusCircle,
-  Settings,
-  User,
-  UserPlus,
-  Users
-} from 'lucide-react';
-
+import {User} from 'lucide-react';
 import {LuGamepad2, LuSkull} from 'react-icons/lu';
 import {GiBootKick} from 'react-icons/gi';
 import {MdOutlineManageAccounts} from 'react-icons/md';
 import {IoVolumeMuteOutline} from 'react-icons/io5';
-import {PiPaperPlaneTiltBold} from 'react-icons/pi';
+import {PiPaperPlaneTiltBold, PiSmileyAngry} from 'react-icons/pi';
+// 여기까진 아이콘 임포트
+
 import AvatarIcon from '@/components/avatar/AvatarIcon';
 import {Button} from '@/components/shadcn/button';
 import {
@@ -35,6 +22,59 @@ import {
   DropdownMenuTrigger
 } from '@/components/shadcn/dropdown-menu';
 
+const UserDropdownGroup = () => {
+  return (
+    <DropdownMenuGroup>
+      <DropdownMenuItem>
+        <User className='mr-2 h-4 w-4' />
+        <span>프로필 보기</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        <LuGamepad2 className='mr-2 h-4 w-4' />
+        <span>일대일 게임</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        <PiSmileyAngry className='mr-2 h-4 w-4' />
+        <span>차단하기</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        <PiPaperPlaneTiltBold className='mr-2 h-4 w-4' />
+        <span>일대일 채팅</span>
+      </DropdownMenuItem>
+    </DropdownMenuGroup>
+  );
+};
+
+const AdminDropdownGroup = () => {
+  return (
+    <DropdownMenuGroup>
+      <DropdownMenuSub>
+        <DropdownMenuSubTrigger>
+          <MdOutlineManageAccounts className='mr-2 h-4 w-4' />
+          <span>유저 관리</span>
+        </DropdownMenuSubTrigger>
+        <DropdownMenuPortal>
+          <DropdownMenuSubContent>
+            <DropdownMenuItem>
+              <LuSkull className='mr-2 h-4 w-4' />
+              <span>재명하기(BAN)</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <GiBootKick className='mr-2 h-4 w-4' />
+              <span>추방하기(KICK)</span>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <IoVolumeMuteOutline className='mr-2 h-4 w-4' />
+              <span>음소거(MUTE)</span>
+            </DropdownMenuItem>
+          </DropdownMenuSubContent>
+        </DropdownMenuPortal>
+      </DropdownMenuSub>
+    </DropdownMenuGroup>
+  );
+};
+
 export default function DropdownAvatarBtn({
   profile_image,
   user_name
@@ -42,7 +82,6 @@ export default function DropdownAvatarBtn({
   profile_image: string;
   user_name: string;
 }) {
-  console.log(profile_image);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -53,50 +92,9 @@ export default function DropdownAvatarBtn({
       <DropdownMenuContent className='w-56'>
         <DropdownMenuLabel>{user_name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <User className='mr-2 h-4 w-4' />
-            <span>프로필 보기</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <LuGamepad2 className='mr-2 h-4 w-4' />
-            <span>일대일 게임</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <IoVolumeMuteOutline className='mr-2 h-4 w-4' />
-            <span>차단하기</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <PiPaperPlaneTiltBold className='mr-2 h-4 w-4' />
-            <span>일대일 채팅</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
+        {UserDropdownGroup()}
         <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>
-              <MdOutlineManageAccounts className='mr-2 h-4 w-4' />
-              <span>유저 관리</span>
-            </DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem>
-                  <LuSkull className='mr-2 h-4 w-4' />
-                  <span>재명하기(BAN)</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <GiBootKick className='mr-2 h-4 w-4' />
-                  <span>추방하기(KICK)</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <PlusCircle className='mr-2 h-4 w-4' />
-                  <span>음소거(MUTE)</span>
-                </DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-        </DropdownMenuGroup>
+        {AdminDropdownGroup()}
       </DropdownMenuContent>
     </DropdownMenu>
   );
