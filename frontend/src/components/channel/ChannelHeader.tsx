0344df9c1fa936e456e18f3a01fb11200376d77f
Hawardshin <wnddms12345@naver.com>
2023-10-11 19:18:06 +0900

    Feat: Init Chat Header - 채팅방 헤더에 버튼들을 diaglog로 만들었음.

--- frontend/src/components/channel/ChannelHeader.tsx
+++ frontend/src/components/channel/ChannelHeader.tsx
@@ -4,7 +4,7 @@ import PublicRoomList from './PublicRoomList';
 
 export default function ChannelHeader() {
   return (
-    <div className='flex h-[100px] bg-white items-baseline space-y-5 '>
+    <div className='flex h-[100px] bg-white items-baseline space-y-5 space-x-12 pl-6 z-10'>
       <PublicRoomList />
       <CreateChannel />
       <ManageChannel />
--- frontend/src/components/channel/ManageChannel.tsx
+++ frontend/src/components/channel/ManageChannel.tsx
@@ -7,22 +7,62 @@ import {
 import {RiChatSettingsLine} from 'react-icons/ri';
 import {Button} from '@/components/shadcn/button';
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/shadcn/dialog';
+
+import {Input} from '@/components/shadcn/input';
+import {Label} from '@/components/shadcn/label';
+
 export default function ManageChannel() {
   return (
-    <TooltipProvider delayDuration={0}>
-      <Tooltip className='sticky top-0'>
-        <TooltipTrigger asChild>
-          <Button
-            className='ml-auto rounded-full h-14'
-            onClick={() => setOpen(true)}
-          >
-            <RiChatSettingsLine className='h-8 w-8' />
-            <p className='text-xl'>채널 관리</p>
-            <span className='sr-only'>Public Room List</span>
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent sideOffset={10}>Public Room List</TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className='ml-auto rounded-full h-14'>
+          <RiChatSettingsLine className='h-8 w-8' />
+          <p className='text-xl'>채널 관리</p>
+          <span className='sr-only'>Public Room List</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[425px]'>
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className='grid gap-4 py-4'>
+          <div className='grid grid-cols-4 items-center gap-4'>
+            <Label htmlFor='name' className='text-right'>
+              Name
+            </Label>
+            <Input
+              id='name'
+              defaultValue='Pedro Duarte'
+              className='col-span-3'
+            />
+          </div>
+          <div className='grid grid-cols-4 items-center gap-4'>
+            <Label htmlFor='username' className='text-right'>
+              Username
+            </Label>
+            <Input
+              id='username'
+              defaultValue='@peduarte'
+              className='col-span-3'
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type='submit'>Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
