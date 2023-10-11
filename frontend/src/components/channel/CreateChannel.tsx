a2bea3010ce79143be825f9c74387e3c7dcc9c52
Hawardshin <wnddms12345@naver.com>
2023-10-11 20:09:36 +0900

    Feat: 채널 생성 모달 구현중

--- frontend/src/components/channel/CreateChannel.tsx
+++ frontend/src/components/channel/CreateChannel.tsx
@@ -11,6 +11,33 @@ import {
   DialogTrigger
 } from '@/components/shadcn/dialog';
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '@/components/shadcn/select';
+
+function SelectRoom({id}: {id: string}) {
+  return (
+    <Select>
+      <SelectTrigger className='w-[275px] '>
+        <SelectValue placeholder='채널 유형 선택' id={id} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>채널 유형 선택</SelectLabel>
+          <SelectItem value='apple'>Public</SelectItem>
+          <SelectItem value='banana'>Private</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
+
 import {Input} from '@/components/shadcn/input';
 import {Label} from '@/components/shadcn/label';
 
@@ -33,15 +60,18 @@ export default function CreateChannel() {
         </DialogHeader>
         <div className='grid gap-4 py-4'>
           <div className='grid grid-cols-4 items-center gap-4'>
+            <Label htmlFor='channel_type' className='text-right'>
+              채널 유형
+            </Label>
+            <SelectRoom id='channel_type' />
+          </div>
+          <div className='grid grid-cols-4 items-center gap-4'>
             <Label htmlFor='name' className='text-right'>
               채널 명
             </Label>
-            <Input
-              id='name'
-              defaultValue='원하는 이름으로 입력해주세요'
-              className='col-span-3'
-            />
+            <Input id='name' className='col-span-3' />
           </div>
+          {/**TODO : 친구 초대 */}
           <div className='grid grid-cols-4 items-center gap-4'>
             <Label htmlFor='passward' className='text-right'>
               passward
