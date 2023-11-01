65af44f768e84aa02e609a41dc7592b67b964de1
wnddms12345 <wnddms12345@naver.com>
2023-11-01 17:36:16 +0900

    Feat: auth code 발급

--- src/pages/login/redirect.tsx
+++ src/pages/login/redirect.tsx
@@ -1,5 +1,9 @@
+import {useEffect} from 'react';
+import {useRouter} from 'next/router';
 export default function Redirect() {
-  
-	
-	return <div></div>;
+  const router = useRouter();
+  console.log(router);
+  const auth_code = router.query.code || [];
+  console.log(auth_code);
+  return <div></div>;
 }
