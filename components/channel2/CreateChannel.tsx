6dd5b2c54433514d3e8f1d3b1d493b609cad171b
Hawardshin <wnddms12345@naver.com>
2023-11-13 10:53:37 +0900

    Feat: Friend List selector and split code

--- components/channel2/CreateChannel.tsx
+++ components/channel2/CreateChannel.tsx
@@ -30,30 +30,138 @@ import {
   DropdownMenuTrigger
 } from '@/components/shadcn/ui/dropdown-menu';
 
+import {Dispatch, SetStateAction} from 'react';
+
 import {useState} from 'react';
 import {Input} from '@/components/shadcn/ui/input';
 import {Label} from '@/components/shadcn/ui/label';
 
+const SelectChannelType = ({channelType, setChannelType}) => {
+  return (
+    <div className='grid grid-cols-4 items-center gap-6'>
+      <Label htmlFor='channel_type' className='text-right'>
+        채널 유형
+      </Label>
+      <Select
+        value={channelType}
+        onValueChange={(type) => {
+          setChannelType(type);
+          console.log(type);
+        }}
+      >
+        <SelectTrigger className=' bg-custom2 w-[342px]'>
+          <SelectValue placeholder='채널 유형 선택' id='channel_type' />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>채널 유형 선택</SelectLabel>
+            <SelectItem value='Public'>Public</SelectItem>
+            <SelectItem value='Private'>Private</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+const ChannelNameInput = ({channelName, setChannelName}) => {
+  return (
+    <div className='grid grid-cols-4 items-center gap-6'>
+      <Label htmlFor='channel_name' className='text-right'>
+        채널 명
+      </Label>
+      <Input
+        className='col-span-3'
+        value={channelName}
+        onChange={(e) => setChannelName(e.target.value)}
+      />
+    </div>
+  );
+};
+
+const InviteFriendSelector = ({inviteFriendList, setInviteFriendList}) => {
+  return (
+    <div className='grid grid-cols-4 items-center gap-6'>
+      <Label htmlFor='description' className='text-right'>
+        친구 초대
+      </Label>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='outline' className='col-span-3 bg-custom2'>
+            친구 초대
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-[280px] max-h-[300px] overflow-auto'>
+          <DropdownMenuLabel> 친구 초대</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {inviteFriendList.map((friendInfo, idx) => {
+            return (
+              <DropdownMenuCheckboxItem
+                key={friendInfo.id}
+                className='col-span-3'
+                checked={friendInfo.checked}
+                onClick={() => {
+                  setInviteFriendList((prev) => {
+                    const newFriendList = [...prev];
+                    newFriendList[idx].checked = !newFriendList[idx].checked;
+                    return newFriendList;
+                  });
+                }}
+              >
+                {friendInfo.name}
+              </DropdownMenuCheckboxItem>
+            );
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+const PasswordInput = ({
+  password,
+  setPassword
+}: {
+  password: string;
+  setPassword: Dispatch<SetStateAction<string>>;
+}) => {
+  return (
+    <div className='grid grid-cols-4 items-center gap-6'>
+      <Label htmlFor='password' className='text-right'>
+        password
+      </Label>
+      <Input id='password' defaultValue='' className='col-span-3' />
+    </div>
+  );
+};
+
 export default function CreateChannel() {
+  // const [friendInfos, setFriendInfos] = useState([] as any);
   const [channelName, setChannelName] = useState('');
   const [channelType, setChannelType] = useState('');
   const [password, setPassword] = useState('');
   const [inviteFriendList, setInviteFriendList] = useState([]);
-  const [friendInfos, setFriendInfos] = useState([] as any);
   const fetchUserInfos = async () => {
     try {
-      const response = await Axios.get(`/users/friends`, {
+      const {data} = await Axios.get(`/users/friends`, {
         params: {id: 'user_id'}
       });
-      console.log(response.data);
-      setFriendInfos(response.data);
+      console.log(data);
+      const results = [];
+      data.forEach((value) => {
+        results.push({
+          name: value.name,
+          id: value.id,
+          checked: false
+        });
+      });
+      setInviteFriendList(results || []);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-
-  const DialogBtn = () => {
-    return (
+  return (
+    <Dialog>
       <DialogTrigger asChild>
         <Button
           className='rounded-full bg-custom1 text-custom4'
@@ -66,12 +174,6 @@ export default function CreateChannel() {
           <span className='sr-only'>Public Room List</span>
         </Button>
       </DialogTrigger>
-    );
-  };
-
-  return (
-    <Dialog>
-      <DialogBtn />
       <DialogContent className='bg-custom1'>
         <DialogHeader>
           <DialogTitle className='text-center'>채널 생성</DialogTitle>
@@ -80,75 +182,19 @@ export default function CreateChannel() {
           </DialogDescription>
         </DialogHeader>
         <div className='grid gap-6 py-4'>
-          <div className='grid grid-cols-4 items-center gap-6'>
-            <Label htmlFor='channel_type' className='text-right'>
-              채널 유형
-            </Label>
-            <Select
-              value={channelType}
-              onValueChange={(type) => {
-                setChannelType(type);
-                console.log(type);
-              }}
-            >
-              <SelectTrigger className=' bg-custom2 w-[342px]'>
-                <SelectValue placeholder='채널 유형 선택' id='channel_type' />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>채널 유형 선택</SelectLabel>
-                  <SelectItem value='Public'>Public</SelectItem>
-                  <SelectItem value='Private'>Private</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className='grid grid-cols-4 items-center gap-6'>
-            <Label htmlFor='channel_name' className='text-right'>
-              채널 명
-            </Label>
-            <Input
-              className='col-span-3'
-              value={channelName}
-              onChange={(e) => setChannelName(e.target.value)}
-            />
-          </div>
-          <div className='grid grid-cols-4 items-center gap-6'>
-            <Label htmlFor='description' className='text-right'>
-              친구 초대
-            </Label>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant='outline' className='col-span-3 bg-custom2'>
-                  친구 초대
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className='w-[280px] max-h-[300px] overflow-auto'>
-                <DropdownMenuLabel> 친구 초대</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                {friendInfos.map((friendInfo, idx) => {
-                  return (
-                    <DropdownMenuCheckboxItem
-                      key={friendInfo.id}
-                      className='col-span-3'
-                      // checked={true}
-                      // onClick={() => {
-                      //   setFriendSelected(friendInfo.id);
-                      // }}
-                    >
-                      {friendInfo.name}
-                    </DropdownMenuCheckboxItem>
-                  );
-                })}
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <div className='grid grid-cols-4 items-center gap-6'>
-            <Label htmlFor='password' className='text-right'>
-              password
-            </Label>
-            <Input id='password' defaultValue='' className='col-span-3' />
-          </div>
+          <SelectChannelType
+            channelType={channelType}
+            setChannelType={setChannelType}
+          />
+          <ChannelNameInput
+            channelName={channelName}
+            setChannelName={setChannelName}
+          />
+          <InviteFriendSelector
+            inviteFriendList={inviteFriendList}
+            setInviteFriendList={setInviteFriendList}
+          />
+          <PasswordInput password={password} setPassword={setPassword} />
         </div>
         <DialogFooter>
           <Button type='submit' className='w-full'>
