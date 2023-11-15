0e63c1d71cd8faea12e96f048b501c69abd7641f
wnddms12345 <wnddms12345@naver.com>
2023-11-15 19:20:16 +0900

    Fix: 배경이 퍼지지 않는 문제 해결

--- components/channel/header/ChannelHeader.tsx
+++ components/channel/header/ChannelHeader.tsx
@@ -4,20 +4,13 @@ import PublicRoomList from './publicRoom/PublicRoomList';
 
 export default function ChannelHeader({channel_name}: {channel_name: string}) {
   return (
-    <div bg-custom2 className='bg-custom2 '>
-      <div className='flex items-baseline justify-between w-full'>
-        <div>
-          <PublicRoomList />
-        </div>
-
-        <div>
-          <CreateChannel />
-        </div>
-
-        {/* <div>
+    <div bg-custom2 className='bg-custom2 w-full'>
+      <PublicRoomList />
+      <CreateChannel />
+      {/* <div>
         <ManageChannel channel_name={channel_name} />
       </div> */}
-      </div>
+
       <div className='font-bold text-3xl text-center'>{channel_name}</div>
     </div>
   );
