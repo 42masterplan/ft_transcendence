66377f81a100247f60eeca358a6c187ae7f925c0
wnddms12345 <wnddms12345@naver.com>
2023-11-17 17:50:58 +0900

    .

--- components/userInfo/SetUserInfo.tsx
+++ components/userInfo/SetUserInfo.tsx
@@ -5,6 +5,7 @@ import {Button} from '../shadcn/ui/button';
 import Axios from '@/api';
 import SetUserName from './SetUserName';
 import {useRouter} from 'next/router';
+
 export default function SetUserInfo() {
   const [nickname, setNickname] = useState('');
   const [isValidName, setIsValidName] = useState(false);
@@ -13,6 +14,7 @@ export default function SetUserInfo() {
     process.env.NEXT_PUBLIC_CHARACTER_HOSTING_URI1 || ''
   );
   const router = useRouter();
+
   const handleSubmit = async () => {
     try {
       await Axios.post('/users', {
@@ -26,55 +28,49 @@ export default function SetUserInfo() {
       alert('회원가입 실패');
     }
   };
-  return (
-    <div className='flex flex-col items-center max-w-100 h-auto rounded-lg overflow-y-auto p-6 bg-custom2/70 gap-3'>
-      <h1
-        className='font-roboto-mono text-4xl
-            font-semibold leading-10 tracking-normal text-custom4 m-3'
-      >
-        회원 정보 설정
-      </h1>
-      <SetUserName
-        nickname={nickname}
-        setNickname={setNickname}
-        isValidName={isValidName}
-        setIsValidName={setIsValidName}
-      />
 
-      <SetAvatar setProfileImage={setProfileImage} />
-      {/* <InputFile /> */}
-      <label
-        htmlFor='statusMsg'
-        className='font-roboto-mono text-1xl font-semibold leading-10 tracking-normal text-custom4'
-      >
-        상태 메시지
-      </label>
-      <Input
-        id='statusMsg'
-        value={statusMsg}
-        onChange={(e) => {
-          if (e.target.value.length > 20) {
-            alert('20자 이하로 입력해주세요.');
-            return;
-          }
-          setStatusMsg(e.target.value);
-        }}
-        placeholder='여러분을 표현해봐요'
-      />
-      <p className='text-custom4 text-xs'>
-        상태 메시지는 최대 20자까지 입력 가능합니다.
-      </p>
-      {/* <LinkBtn
-        link='/welcome/register/2step-auth'
-        className='bg-custom4 '
-        disabled={isValidName !== true}
-        onClick={() => {}}
-      >
-        계속하기
-      </LinkBtn> */}
-      <Button disabled={isValidName !== true} onClick={handleSubmit}>
-        계속하기
-      </Button>
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='max-w-100 rounded-lg overflow-y-auto p-6 bg-custom2/70 gap-3'>
+        <h1 className='font-roboto-mono text-4xl font-semibold leading-10 tracking-normal text-custom4 m-3'>
+          회원 정보 설정
+        </h1>
+        <SetUserName
+          nickname={nickname}
+          setNickname={setNickname}
+          isValidName={isValidName}
+          setIsValidName={setIsValidName}
+        />
+        <SetAvatar setProfileImage={setProfileImage} />
+        <label
+          htmlFor='statusMsg'
+          className='font-roboto-mono text-1xl font-semibold leading-10 tracking-normal text-custom4'
+        >
+          상태 메시지
+        </label>
+        <Input
+          id='statusMsg'
+          value={statusMsg}
+          onChange={(e) => {
+            if (e.target.value.length > 20) {
+              alert('20자 이하로 입력해주세요.');
+              return;
+            }
+            setStatusMsg(e.target.value);
+          }}
+          placeholder='여러분을 표현해봐요'
+        />
+        <p className='text-custom4 text-xs p-3'>
+          상태 메시지는 최대 20자까지 입력 가능합니다.
+        </p>
+        <Button
+          className='self-center w-full mt-4 '
+          disabled={isValidName !== true}
+          onClick={handleSubmit}
+        >
+          계속하기
+        </Button>
+      </div>
     </div>
   );
 }
