2d686dd8aea7fae86db4744579838ba4bdb874d2
Hawardshin <wnddms12345@naver.com>
2023-11-18 22:57:37 +0900

    Feat: Two Factor Validation

--- pages/welcome/2step-auth/index.tsx
+++ pages/welcome/2step-auth/index.tsx
@@ -1,12 +1,16 @@
 import LinkBtn from '@/components/button/LinkBtn';
-import {InputWithLabel} from '@/components/input/InputWithLabel';
+
+import {Button} from '@/components/shadcn/ui/button';
 import Title from '@/components/Title';
 import Image from 'next/image';
-// import bg from '@/public/space_dog.jpg';
+import {Input} from '@/components/shadcn/ui/input';
+import {Label} from '@/components/shadcn/ui/label';
+import {useState} from 'react';
 export default function twoStepAuth() {
+  const [email, setEmail] = useState('');
   return (
     <>
-      <div className=''>
+      <div>
         <Title />
         <div className='flex flex-col items-center w-full h-auto rounded-lg p-6  gap-3'>
           <h1
@@ -15,19 +19,29 @@ export default function twoStepAuth() {
           >
             2단계 인증
           </h1>
-          {/* <Image
-            src='/space_dog.jpg'
-            width={200}
-            height={200}
+          <Image
+            src='/space_dog.png'
+            width={400}
+            height={400}
             alt='2단계 인증'
-          /> */}
-          <InputWithLabel
-            header='2단계 인증에 필요한 Email'
-            placeholder='당신의 Email이 필요해요'
+            className='h-1/3 w-1/3'
           />
-          <LinkBtn link='/welcome/2step-auth/validation'>
+          <div className='grid w-full max-w-sm items-center gap-1.5  m-3'>
+            <Label htmlFor='text'>2단계 인증에 필요한 Email</Label>
+            <Input
+              type='text'
+              id='text'
+              placeholder='당신의 Email이 필요해요'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <Button size='lg' variant='default'>
             인증 코드 받기
-          </LinkBtn>
+          </Button>
+          {/* <LinkBtn link='/welcome/2step-auth/validation'>
+            인증 코드 받기
+          </LinkBtn> */}
         </div>
       </div>
     </>
