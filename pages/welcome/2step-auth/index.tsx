1b608969a914938403ef23d2377f8b4890008455
Hawardshin <wnddms12345@naver.com>
2023-11-18 22:08:12 +0900

    Feat: Change new image

--- pages/welcome/2step-auth/index.tsx
+++ pages/welcome/2step-auth/index.tsx
@@ -1,19 +1,27 @@
 import LinkBtn from '@/components/button/LinkBtn';
 import {InputWithLabel} from '@/components/input/InputWithLabel';
 import Title from '@/components/Title';
-
+import Image from 'next/image';
+// import bg from '@/public/space_dog.jpg';
 export default function twoStepAuth() {
   return (
     <>
-      <div className='flex-col justify-center'>
+      <div className=''>
         <Title />
-        <div className='flex flex-col items-center w-[466px] h-auto rounded-lg p-6 bg-custom2 gap-3'>
+
+        <div className='flex flex-col items-center w-full h-auto rounded-lg p-6  gap-3'>
           <h1
             className='font-roboto-mono text-4xl
             font-semibold leading-10 tracking-normal text-center m-3 '
           >
             2단계 인증
           </h1>
+          {/* <Image
+            src='/space_dog.jpg'
+            width={200}
+            height={200}
+            alt='2단계 인증'
+          /> */}
           <InputWithLabel
             header='2단계 인증에 필요한 Email'
             placeholder='당신의 Email이 필요해요'
