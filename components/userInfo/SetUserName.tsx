f0d3a3deb47e8828bcff26a47283c49f676cc4bc
Hawardshin <wnddms12345@naver.com>
2023-11-20 19:35:41 +0900

    Feat: Delete Comment

--- components/userInfo/SetUserName.tsx
+++ components/userInfo/SetUserName.tsx
@@ -1,80 +1,8 @@
 import {Input} from '../shadcn/ui/input';
 import {Button} from '../shadcn/ui/button';
-
-import Axios from '@/api';
 import {Dispatch, SetStateAction, useEffect} from 'react';
 import {useToast} from '../shadcn/ui/use-toast';
 import useAxios from '@/hooks/useAxios';
-// function checkNickname(
-//   nickname: string,
-//   setIsValidName: Dispatch<SetStateAction<boolean>>,
-//   toast: any,
-//   fetch: any,
-//   response: any
-// ) {
-//   //이거 api 바꾸면 이걸로 바꿀 예정입니다.
-//   if (nickname.length <= 2) {
-//     toast({
-//       title: '닉네임은 3자 이상 입력해주세요.',
-//       variant: 'destructive'
-//     });
-//     return;
-//   }
-//   fetch({
-//     method: 'get',
-//     url: '/users/is-duplicated-name',
-//     params: {name: nickname}
-//   });
-//   if (response.data == true) {
-//     toast({
-//       title: '이미 존재하는 닉네임입니다.',
-//       description: '다른 닉네임을 입력해주세요.',
-//       variant: 'destructive',
-//       duration: 3000
-//     });
-//     setIsValidName(false);
-//   } else {
-//     toast({
-//       title: '사용 가능한 닉네임입니다.',
-//       description: '이 닉네임으로 진행해주세요.',
-//       variant: 'success',
-//       duration: 3000
-//     });
-//     setIsValidName(true);
-//   }
-// try {
-//   const res = await Axios.get('/users/is-duplicated-name', {
-//     params: {name: nickname}
-//   });
-//   console.log(res);
-//   if (res.data == true) {
-//     toast({
-//       title: '이미 존재하는 닉네임입니다.',
-//       description: '다른 닉네임을 입력해주세요.',
-//       variant: 'destructive',
-//       duration: 3000
-//     });
-//     setIsValidName(false);
-//   } else {
-//     toast({
-//       title: '사용 가능한 닉네임입니다.',
-//       description: '이 닉네임으로 진행해주세요.',
-//       variant: 'success',
-//       duration: 3000
-//     });
-//     setIsValidName(true);
-//   }
-// } catch (err) {
-//   console.log(err);
-//   setIsValidName(false);
-//   toast({
-//     title: '닉네임 중복 확인 실패',
-//     description: '닉네임 중복 확인에 실패했습니다.',
-//     variant: 'destructive',
-//     duration: 3000
-//   });
-// }
-// }
 
 export default function SetUserName({
   nickname,
@@ -112,7 +40,7 @@ export default function SetUserName({
       });
       setIsValidName(true);
     }
-  }, [response]);
+  }, [response, toast, setIsValidName]);
   const checkNickname = () => {
     if (nickname.length <= 2) {
       toast({
