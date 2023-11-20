2d59e4238109417f7a247bb89c5c06623e30fbec
Seojun Yoo <seojunyoo@Seojuns-MacBook-Air.local>
2023-11-20 17:19:25 +0900

    Feat: MessageInputBar improved. Now can get multiple line.

--- components/input/MessageInputBar.tsx
+++ components/input/MessageInputBar.tsx
@@ -13,7 +13,6 @@
  */
 
 import {Button} from '@/components/shadcn/ui/button';
-import {Input} from '@/components/shadcn/ui/input';
 import {ArrowUp, Loader} from 'lucide-react';
 
 interface MessageInputBarProps {
@@ -34,30 +33,31 @@ export default function MessageInputBar({
   handleSendMsg
 }: MessageInputBarProps) {
   const handleKeyPress = async (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter' && msg.trim().length > 0) {
-      e.preventDefault(); // Prevents the default action of the enter key
-      setIsSending(true);
-      await handleSendMsg().then(() => {
-        setIsSending(false);
-        setMsg('');
-      });
+    if (e.key === 'Enter' && !e.shiftKey) {
+      if (msg.trim().length > 0) {
+        e.preventDefault(); // Prevent the default action of the enter key
+        setIsSending(true);
+        await handleSendMsg().then(() => {
+          setIsSending(false);
+          setMsg('');
+        });
+      }
     }
   };
 
   return (
     <div className={`w-full p-3 ${className}`}>
       <div className='flex flex-row gap-3 justify-between items-center w-full h-20 p-2 sm:h-24 bg-custom2 overflow-hidden rounded-3xl'>
-        <Input
+        <textarea
           id='DMMessage'
-          type='text'
           value={msg}
           placeholder='Type a message...'
-          className='w-full h-full text-2xl bg-transparent border-none rounded-3xl'
+          className='w-full h-full text-2xl bg-transparent border-none rounded-3xl resize-none p-2 ' // resize-none to prevent resizing
           onChange={(e) => setMsg(e.target.value)}
           onKeyDown={handleKeyPress}
         />
         {msg.length > 0 ? (
-          <div className='flex fle-row justify-center items-center w-16 h-full'>
+          <div className='flex flex-row justify-center items-center w-16 h-full'>
             <Button
               variant='default'
               className='w-14 h-14 rounded-full'
