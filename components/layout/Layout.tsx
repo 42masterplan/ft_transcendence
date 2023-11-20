61ea2b541732e178bf2cd5e40d90f916e7128faf
daejlee <savif4991@gmail.com>
2023-11-20 15:45:47 +0900

    Feat: Hide layout when game active

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,21 +1,29 @@
 import Header from './header/Header';
 import Footer from './footer/Footer';
 import {Toaster} from '@/components/shadcn/ui/toaster';
-
+import {useRouter} from 'next/router';
 export default function Layout({children}: {children: React.ReactNode}) {
+  const router = useRouter();
   return (
     <>
-      <Header
-        className={`flex flex-row w-full items-center justify-between px-3 sm:px-4 h-[5vh] sm:h-[7vh]`}
-      />
+      {router.pathname.startsWith('/welcome') ||
+      router.pathname.startsWith('/game/active') ? null : (
+        <Header
+          className={`flex flex-row w-full items-center justify-between px-3 sm:px-4 h-[5vh] sm:h-[7vh]`}
+        />
+      )}
+
       <main
         className={`flex flex-col h-[95vh] sm:h-[86vh] w-full sm:max-w-5xl`}
       >
         {children}
       </main>
-      <Footer
-        className={`hidden sm:flex flex-row w-full items-center justify-between gap-4 px-3 h-[7vh]`}
-      />
+      {router.pathname.startsWith('/welcome') ||
+      router.pathname.startsWith('/game/active') ? null : (
+        <Footer
+          className={`hidden sm:flex flex-row w-full items-center justify-between gap-4 px-3 h-[7vh]`}
+        />
+      )}
       <Toaster />
     </>
   );
