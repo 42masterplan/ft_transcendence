64f0b0f0669fe1a28c8abd0bdfeba43c367c5bd4
Hawardshin <wnddms12345@naver.com>
2023-11-25 19:34:54 +0900

    Feat: Social edting..

--- pages/social/index.tsx
+++ pages/social/index.tsx
@@ -1,6 +1,5 @@
 import * as API from '@/DummyBackend/socialAPI';
 
-import * as React from 'react';
 import {
   socialPageTargetUser as target,
   socialPageUserStatus as status
@@ -8,62 +7,54 @@ import {
 import {SocialPageNavBar} from '@/components/social/SocialPageNavBar';
 import UserCardSection from '@/components/social/UserCardSection';
 import SpinningLoader from '@/components/loader/SpinningLoader';
-
+import useAxios from '@/hooks/useAxios';
+import {useEffect, useState} from 'react';
 export default function SocialPage() {
   // search options
-  const [searchTarget, setSearchTarget] = React.useState<target>('friend');
-  const [searchTargetStatus, setSearchTargetStatus] =
-    React.useState<status>('All');
-  const [searchTargetInput, setSearchTargetInput] = React.useState<string>('');
-
+  const [searchTarget, setSearchTarget] = useState<target>('friend');
+  const [searchTargetStatus, setSearchTargetStatus] = useState<status>('All');
+  const [searchTargetInput, setSearchTargetInput] = useState('');
+  const {fetchData, response, isSuccess} = useAxios();
   // user data
-  const [users, setUsers] = React.useState<API.user[]>([]);
-  async function getUserData() {
-    const users = await API.social__getUsersAsync();
-    setUsers(users);
-  }
-  getUserData();
+  const [users, setUsers] = useState<API.user[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchData({
+      method: 'get',
+      url: '/users/friends',
+      params: {
+        id: '1'
+      },
+      errorTitle: '유저 정보 조회 실패',
+      errorDescription: '유저 정보 조회에 실패했습니다.'
+    });
+  }, []);
+  useEffect(() => {
+    if (isSuccess === true) {
+      setUsers(response.data);
+      setLoading(false);
+      console.log(response.data);
+    }
+  }, [isSuccess, response]);
 
   // TEST: TODO: erase this ----------------------------------------------------
-
-  console.log('render social page');
-  React.useEffect(() => {
-    console.log(
-      "SocialPage's useEffect[searchTarget, searchTargetStatus, searchTargetInput]"
-    );
-    console.log('Search target: ', searchTarget);
-    console.log('Search target status: ', searchTargetStatus);
-    console.log('Search target input: ', searchTargetInput);
-  }, [searchTarget, searchTargetStatus, searchTargetInput]);
-  React.useEffect(() => {
-    console.log("SocialPage's useEffect[users]");
-    console.log('Users: ', users);
-  }, [users]);
-
-  // TEST: TODO: erase this ----------------------------------------------------
-
+  if (loading == true) return <SpinningLoader />;
   return (
-    <>
-      <div className='flex flex-col w-full h-full px-3 gap-2'>
-        <SocialPageNavBar
-          searchTarget={searchTarget}
-          setSearchTarget={setSearchTarget}
-          searchTargetStatus={searchTargetStatus}
-          setSearchTargetStatus={setSearchTargetStatus}
-          searchTargetInput={searchTargetInput}
-          setSearchTargetInput={setSearchTargetInput}
-        />
-        {users.length > 0 ? (
-          <UserCardSection
-            users={users}
-            searchTarget={searchTarget}
-            searchTargetStatus={searchTargetStatus}
-            searchTargetInput={searchTargetInput}
-          />
-        ) : (
-          <SpinningLoader />
-        )}
-      </div>
-    </>
+    <div className='flex flex-col w-full h-full px-3 gap-2'>
+      {/* <SocialPageNavBar
+        searchTarget={searchTarget}
+        setSearchTarget={setSearchTarget}
+        searchTargetStatus={searchTargetStatus}
+        setSearchTargetStatus={setSearchTargetStatus}
+        searchTargetInput={searchTargetInput}
+        setSearchTargetInput={setSearchTargetInput}
+      />
+      <UserCardSection
+        users={users}
+        searchTarget={searchTarget}
+        searchTargetStatus={searchTargetStatus}
+        searchTargetInput={searchTargetInput}
+      /> */}
+    </div>
   );
 }
