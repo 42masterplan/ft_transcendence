f49b0d1aca4e1dac4153eff65107bfc1b8cc44fc
Hawardshin <wnddms12345@naver.com>
2023-11-25 17:22:05 +0900

    Feat: Login Filter

--- components/layout/LoginFilter.tsx
+++ components/layout/LoginFilter.tsx
@@ -12,29 +12,58 @@ export default function LoginFilter({children}: {children: React.ReactNode}) {
     const token = cookie.accessToken;
     const isTwoFactorDone = cookie.isTwoFactorDone;
     const hasAccount = cookie.hasAccount;
-    if (!token || hasAccount === undefined) {
-      removeCookie('accessToken');
-      removeCookie('isTwoFactorDone');
-      removeCookie('hasAccount');
-      toast({
-        title: '로그인 상태 만료',
-        description: '로그인 상태가 만료되었습니다. 다시 로그인 해주세요'
-      });
+    if (router.pathname.startsWith('/welcome')) {
+      if (
+        router.pathname === '/welcome' ||
+        router.pathname === '/welcome/redirect'
+      ) {
+        setLoading(false);
+        return;
+      }
+      if (token || isTwoFactorDone === undefined || hasAccount === undefined)
+        router.push('/welcome');
+      else if (router.pathname === '/welcome/2step-auth') {
+        if (isTwoFactorDone) {
+          toast({
+            title: '이미 인증 완료',
+            description: '이미 2단계 인증이 완료되었습니다'
+          });
+          router.push('/');
+        }
+      } else if (router.pathname === '/welcome/register') {
+        if (hasAccount) {
+          toast({
+            title: '이미 계정 생성 완료',
+            description: '이미 계정이 생성되었습니다'
+          });
+          router.push('/');
+        }
+      }
+    } else {
+      if (!token || hasAccount === undefined) {
+        removeCookie('accessToken');
+        removeCookie('isTwoFactorDone');
+        removeCookie('hasAccount');
+        toast({
+          title: '로그인 상태 만료',
+          description: '로그인 상태가 만료되었습니다. 다시 로그인 해주세요'
+        });
 
-      router.push('/welcome');
-    }
-    if (hasAccount === false) {
-      router.push('/welcome/register');
-      toast({
-        title: '계정 생성 필요',
-        description: '계정이 생성되지 않았습니다. 계정을 생성해주세요'
-      });
-    } else if (isTwoFactorDone === false) {
-      router.push('/welcome/2step-auth');
-      toast({
-        title: '2단계 인증 필요',
-        description: '2단계 인증이 필요합니다. 2단계 인증을 진행해주세요'
-      });
+        router.push('/welcome');
+      }
+      if (hasAccount === false) {
+        router.push('/welcome/register');
+        toast({
+          title: '계정 생성 필요',
+          description: '계정이 생성되지 않았습니다. 계정을 생성해주세요'
+        });
+      } else if (isTwoFactorDone === false) {
+        router.push('/welcome/2step-auth');
+        toast({
+          title: '2단계 인증 필요',
+          description: '2단계 인증이 필요합니다. 2단계 인증을 진행해주세요'
+        });
+      }
     }
     setLoading(false);
   }, [router.pathname]);
