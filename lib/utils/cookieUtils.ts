230fcc851d1d11d20527fe925bb7c066b5c4d25d
Hawardshin <wnddms12345@naver.com>
2023-12-26 16:00:48 +0900

    Fix: document in ServerSide fix

--- lib/utils/cookieUtils.ts
+++ lib/utils/cookieUtils.ts
@@ -3,13 +3,13 @@
 const getAuthorization = (): string | null => {
   const cookies: {[key: string]: string} = {};
   // console.log('쿠키:', document.cookie);
-  if (typeof document !== undefined) {
-    document.cookie.split(';').forEach((cookie) => {
+  if (typeof document === 'undefined') return null;
+  else {
+    document?.cookie?.split(';').forEach((cookie) => {
       const [name, value] = cookie.trim().split('=');
       cookies[name] = value;
     });
     // console.log('쿠키22:', cookies);
-
     const token = cookies.accessToken || null;
     // console.log('토큰22222:', token);
     return token;
