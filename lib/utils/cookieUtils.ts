ada9bad2368e673363db1f2e9e2b71a9a2adb654
Hawardshin <wnddms12345@naver.com>
2023-12-26 14:07:01 +0900

    Feat: JWT with Socket

--- lib/utils/cookieUtils.ts
+++ lib/utils/cookieUtils.ts
@@ -1,15 +1,19 @@
+'use client';
+
 const getAuthorization = (): string | null => {
   const cookies: {[key: string]: string} = {};
-  console.log('쿠키:', document.cookie);
-  document.cookie.split(';').forEach((cookie) => {
-    const [name, value] = cookie.trim().split('=');
-    cookies[name] = value;
-  });
-  console.log('쿠키22:', cookies);
+  // console.log('쿠키:', document.cookie);
+  if (typeof document !== undefined) {
+    document.cookie.split(';').forEach((cookie) => {
+      const [name, value] = cookie.trim().split('=');
+      cookies[name] = value;
+    });
+    // console.log('쿠키22:', cookies);
 
-  const token = cookies.accessToken || null;
-  // console.log('토큰22222:', token);
-  return token;
+    const token = cookies.accessToken || null;
+    // console.log('토큰22222:', token);
+    return token;
+  }
 };
 
 export default getAuthorization;
