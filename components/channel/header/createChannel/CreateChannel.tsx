b2f0434b5dd6f98d886537ca072809094a069ad8
Hawardshin <wnddms12345@naver.com>
2023-12-29 22:28:11 +0900

    Feat: Set Create Channel Name Passward Limit

--- components/channel/header/createChannel/CreateChannel.tsx
+++ components/channel/header/createChannel/CreateChannel.tsx
@@ -148,7 +148,9 @@ const PasswordInput = ({
       <Input
         id='password'
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
         className='col-span-3'
       />
     </div>
@@ -185,10 +187,16 @@ export default function CreateChannel() {
     }
   };
   const createChannel = () => {
-    if (channelName.length === 0 || channelType.length === 0) {
+    if (
+      channelName.length === 0 ||
+      channelType.length === 0 ||
+      channelName.length > 16 ||
+      password.length > 16
+    ) {
       toast({
         title: '채널 생성 실패',
-        description: '채널 이름과 채널 유형을 입력해주세요.',
+        description:
+          '채널 이름과 채널 유형을 입력해주세요! 채널명과 비밀번호의 최대 길이는 16자입니다.(비밀번호는 선택사항입니다.)',
         variant: 'destructive',
         duration: 3000
       });
