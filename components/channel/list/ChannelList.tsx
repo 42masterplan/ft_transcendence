b0e5e503ad6d78f950c5a11bebbf51e34b0afdf0
Hawardshin <wnddms12345@naver.com>
2023-12-30 21:56:34 +0900

    Feat: Channel추방 기능 구현 완

--- components/channel/list/ChannelList.tsx
+++ components/channel/list/ChannelList.tsx
@@ -2,11 +2,12 @@ import {Button} from '@/components/shadcn/ui/button';
 import useSocket from '@/hooks/useSocket';
 import {EngagedChannelType} from '@/types/channel';
 import {cn} from '@/lib/utils';
-import {MsgHistoryType, channelStateType} from '@/types/channel';
+import {channelStateType, MsgHistoryType} from '@/types/channel';
 import React from 'react';
 import {useEffect, Dispatch, SetStateAction, useCallback} from 'react';
 import {useRouter} from 'next/router';
 import {useToast} from '@/components/shadcn/ui/use-toast';
+
 export default React.forwardRef(function ChannelList(
   {
     channelInfoState,
@@ -27,7 +28,7 @@ export default React.forwardRef(function ChannelList(
       type: 'ENGAGED_SET',
       payload: data
     });
-
+    console.log('마이채널 핸들러');
     if (data.length > 0) {
       //current.channelId is not in data?
       for (const befChannel of ref.current.engagedChannels) {
@@ -47,13 +48,15 @@ export default React.forwardRef(function ChannelList(
           infoDispatch({
             type: 'CHANNEL_LEAVE'
           });
+          ref.current.channelId = '';
+          ref.current.engagedChannels = data;
           break;
         }
       }
       ref.current.engagedChannels = data;
     }
   }, []);
-  const channelHistoryHandler = useCallback((data: ChannelHistoryType[]) => {
+  const channelHistoryHandler = useCallback((data: MsgHistoryType[]) => {
     messageDispatch({
       type: 'MESSAGE_SET',
       payload: data
@@ -81,11 +84,14 @@ export default React.forwardRef(function ChannelList(
   }, []);
   useEffect(() => {
     socket.on('myChannels', myChannelsListener);
-    socket.emit('myChannels');
+
     return () => {
       socket.off('myChannels', myChannelsListener);
     };
-  }, [router.pathname]);
+  }, []);
+  useEffect(() => {
+    socket.emit('myChannels');
+  }, [router.pathname, socket]);
   return (
     <div className='flex flex-col min-w-[100px] h-full border overflow-y-scroll rounded-l-xl bg-custom2 w-[20vw] max-w-[300px]'>
       <div className='min-h-[50px] text-l text-center sticky top-0 z-20 bg-custom2 flex justify-center items-center'>
