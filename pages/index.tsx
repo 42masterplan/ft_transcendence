556748aedf6f0a03954a630353559de85ac9d43f
Seojun Yoo <honeybeei@Seojuns-MacBook-Air.local>
2024-01-01 20:49:57 +0900

    Feat: main page redirection removed.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,32 @@ import Router from 'next/router';
 import useAxios from '@/hooks/useAxios';
 import {useEffect, useState} from 'react';
 import SpinningLoader from '@/components/loader/SpinningLoader';
+import {User} from '@/lib/classes/User';
+import ScrollableContainer from '@/components/container/ScrollableContainer';
+import UserInfoCard from '@/components/card/userInfoCard/UserInfoCard';
+import GameStatsSection from '@/components/sections/userInfoPage/GameStatsSection';
+import AchievementSection from '@/components/sections/userInfoPage/AchievementSection';
+import MatchHistorySection from '@/components/sections/userInfoPage/MatchHistorySection';
 
 export default function HomePage() {
   const router = Router;
-  const {fetchData, response, isSuccess, loading, error} = useAxios();
+  // userName
+  const {
+    fetchData: fetchUserName,
+    response: responseUserName,
+    isSuccess: isSuccessUserName,
+    loading: loadingUserName,
+    error: errorUserName
+  } = useAxios();
+  // user info
+  const {
+    fetchData: fetchUserInfo,
+    response: responseUserInfo,
+    isSuccess: isSuccessUserInfo,
+    loading: loadingUserInfo,
+    error: errorUserInfo
+  } = useAxios();
+  const [currentUser, setCurrentUser] = useState<User>(new User());
 
   // fetch data from server ----------------------------------------------------
 
@@ -22,7 +44,7 @@ export default function HomePage() {
    * * error: 500 if server error
    */
   useEffect(() => {
-    fetchData({
+    fetchUserName({
       method: 'get',
       url: '/users/myName',
       errorTitle: '유저 정보 조회 실패',
@@ -30,22 +52,80 @@ export default function HomePage() {
     });
   }, []);
   useEffect(() => {
-    if (error === true) {
+    if (errorUserName === true) {
       // user not found -> redirect to 404 page
       router.push('/404');
     }
-    if (isSuccess === true) {
-      // when user is found -> redirect to user info page
-      router.push(`/userInfo/${response.name}`);
+    if (isSuccessUserName === true) {
+      // when user is found -> fetch user info
+      fetchUserInfo({
+        method: 'get',
+        url: '/users/info',
+        params: {
+          name: responseUserName
+        },
+        errorTitle: '유저 정보 조회 실패',
+        errorDescription: '유저 정보 조회에 실패했습니다.'
+      });
     }
-  }, [isSuccess, response]);
+  }, [errorUserName, isSuccessUserName, responseUserName]);
 
-  return (
-    <>
-      <div>
-        <SpinningLoader />
-        <h2>This page will be redirected to your information page.</h2>
-      </div>
-    </>
-  );
+  useEffect(() => {
+    if (errorUserInfo === true) {
+      // user not found -> redirect to 404 page
+      router.push('/404');
+    }
+    if (isSuccessUserInfo === true) {
+      // when user is found
+      currentUser.id = responseUserInfo.id;
+      currentUser.name = responseUserInfo.name;
+      currentUser.profileImage = responseUserInfo.profileImage;
+      currentUser.currentStatus = responseUserInfo.currentStatus;
+      currentUser.introduction = responseUserInfo.introduction;
+      setCurrentUser(currentUser);
+    }
+  }, [isSuccessUserInfo, responseUserInfo]);
+
+  const animation = 'm-2 hover:scale-[1.02] duration-200 hover:-translate-y-1';
+
+  // render --------------------------------------------------------------------
+
+  if (loadingUserInfo === true) {
+    return (
+      <>
+        <div className='flex w-full items-center justify-center'>
+          <SpinningLoader />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <ScrollableContainer className='w-full max-w-5xl'>
+          <div className='p-4'>
+            <UserInfoCard
+              userInfo={currentUser}
+              size='lg'
+              side='left'
+              printIntro
+              showStatus={false}
+              stretch
+              className={`${animation}`}
+            />
+          </div>
+          <GameStatsSection userName={currentUser.name} className={animation} />
+          <div className='flex flex-col sm:flex-row gap-3 w-full'>
+            <AchievementSection
+              userName={currentUser.name}
+              className={`flex-1 ${animation}`}
+            />
+            <MatchHistorySection
+              userName={currentUser.name}
+              className={`flex-1 ${animation}`}
+            />
+          </div>
+        </ScrollableContainer>
+      </>
+    );
+  }
 }
